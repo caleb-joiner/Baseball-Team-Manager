ae7495153a227e0945734c3a110c2c5b18338676
Caleb Joiner <jcjoiner555@gmail.com>
2022-10-31 07:05:44 -0500

    added a menu and control statements for the menu

--- lineup.py
+++ lineup.py
@@ -1,13 +1,21 @@
 print("=============================================================")
 print("Baseball Team Manager")
-print("This program calculates the batting average for a player ")
-print("based on the player's official number of at bats and hits.")
+print("MENU OPTIONS")
+print("1 - Calculate batting average")
+print("2 - Exit program")
 print("=============================================================")
 print()
 
-name = input("Enter player's name: ")
-at_bats = int(input("Enter player's at bats: "))
-hits = int(input("Enter player's hits: "))
-average = round(hits / at_bats, 3)
+user_choice = int(input("Enter a menu option: "))
+if user_choice == 1:
+    name = input("Enter player's name: ")
+    at_bats = int(input("Enter player's at bats: "))
+    hits = int(input("Enter player's hits: "))
+    average = round(hits / at_bats, 3)
 
-print(f"{name}'s batting average is {average}")
+    print(f"{name}'s batting average is {average}")
+elif user_choice == 2:
+    print("Goodbye.")
+    exit()
+else:
+    print("Not a valid option. Please try again.")
