0350b4f544b7d33bb9e51b6994031e76af76c1d4
Caleb Joiner <jcjoiner555@gmail.com>
2022-10-31 07:11:14 -0500

    added functions to organize the code and make it more reusable

--- lineup.py
+++ lineup.py
@@ -1,21 +1,30 @@
-print("=============================================================")
-print("Baseball Team Manager")
-print("MENU OPTIONS")
-print("1 - Calculate batting average")
-print("2 - Exit program")
-print("=============================================================")
-print()
+def main_menu():
+    print("=============================================================")
+    print("Baseball Team Manager")
+    print("MENU OPTIONS")
+    print("1 - Calculate batting average")
+    print("2 - Exit program")
+    print("=============================================================")
+    print()
 
-user_choice = int(input("Enter a menu option: "))
-if user_choice == 1:
-    name = input("Enter player's name: ")
-    at_bats = int(input("Enter player's at bats: "))
-    hits = int(input("Enter player's hits: "))
-    average = round(hits / at_bats, 3)
+def calc_avg():
+    user_choice = int(input("Enter a menu option: "))
+    if user_choice == 1:
+        name = input("Enter player's name: ")
+        at_bats = int(input("Enter player's at bats: "))
+        hits = int(input("Enter player's hits: "))
+        average = round(hits / at_bats, 3)
 
-    print(f"{name}'s batting average is {average}")
-elif user_choice == 2:
-    print("Goodbye.")
-    exit()
-else:
-    print("Not a valid option. Please try again.")
+        print(f"{name}'s batting average is {average}")
+    elif user_choice == 2:
+        print("Goodbye.")
+        exit()
+    else:
+        print("Not a valid option. Please try again.")
+        calc_avg()
+
+def main():
+    main_menu()
+    calc_avg()
+
+main()
